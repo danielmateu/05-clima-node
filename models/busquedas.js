c3d363b656ae477184bde31f69e22058a939a783
Daniel Mateu <danielmateu86@gmail.com>
2022-11-07 10:12:38 +0100

    Crear instancias a Mapbox

--- models/busquedas.js
+++ models/busquedas.js
@@ -9,12 +9,25 @@ class Busquedas {
         //TODO: leer db si existe
     }
 
+    get paramsMapbox () {
+        return {
+            'access_token': 'pk.eyJ1IjoiZGFuaWRldi1iY24iLCJhIjoiY2xhNml6NzBtMDFnMTNvbXF4bWs3M3BzdyJ9.KVnHCs7UweqcqiKo5wFObA',
+            'limit': 5,
+            'lenguage': 'es'
+        }
+    }
+
     async ciudad(lugar = '' ){
 
         try {
                     //peticion http
-                    // console.log('ciudad' ,lugar);
-                    const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Barcelona.json?proximity=ip&types=place%2Cpostcode%2Caddress%2Ccountry%2Cregion%2Cdistrict%2Clocality%2Cneighborhood%2Cpoi&language=es&access_token=pk.eyJ1IjoiZGFuaWRldi1iY24iLCJhIjoiY2xhNmphbDEyMWt6aDNvcGhqOTZxeDJsNCJ9.mXy9jTDDTQtm2Qr4U5r2mg&limit=5');
+                    const instance = axios.create({
+                        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json`,
+                        params: this.paramsMapbox
+                    });
+
+                    const resp = await instance.get();
+                    
                     console.log(resp.data);
             
                     return []; // Retornar los lugares ue coincidan con lo introducido
