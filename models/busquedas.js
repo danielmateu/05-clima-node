bed1bc95cc8da20635e3ed852ab524ece678c279
Daniel Mateu <danielmateu86@gmail.com>
2022-11-07 09:54:18 +0100

    Conexion a Mapbox

--- models/busquedas.js
+++ models/busquedas.js
@@ -14,7 +14,7 @@ class Busquedas {
         try {
                     //peticion http
                     // console.log('ciudad' ,lugar);
-                    const resp = await axios.get('https://reqres.in/api/users?page=2');
+                    const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Barcelona.json?proximity=ip&types=place%2Cpostcode%2Caddress%2Ccountry%2Cregion%2Cdistrict%2Clocality%2Cneighborhood%2Cpoi&language=es&access_token=pk.eyJ1IjoiZGFuaWRldi1iY24iLCJhIjoiY2xhNmphbDEyMWt6aDNvcGhqOTZxeDJsNCJ9.mXy9jTDDTQtm2Qr4U5r2mg&limit=5');
                     console.log(resp.data);
             
                     return []; // Retornar los lugares ue coincidan con lo introducido
